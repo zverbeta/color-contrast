b5534a26917a94d8beab647ab9ef476397359da8
Yaroslav Shutko <shutko.yaroslav@gmail.com>
2018-09-03 16:14:31 +0300

    Add old svg icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
+import { DomSanitizer } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+
+import { MatIconRegistry } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +12,17 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    private iconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer,
+  ) {
+    iconRegistry.addSvgIconSet(domSanitizer.bypassSecurityTrustResourceUrl('assets/icons-set.svg'));
+  }
+}
