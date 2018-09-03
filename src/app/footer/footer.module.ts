4f0cb82cafc52e7c04781d3581652165cee188a3
zverbeta <shutko.yaroslav@gmail.com>
2018-09-04 01:58:25 +0300

    Add footer gituhb link, mail link

--- src/app/footer/footer.module.ts
+++ src/app/footer/footer.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatIconModule
   ],
   declarations: [
     FooterComponent
